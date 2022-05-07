a84cc88fd56aa9f03e26a43f4e1f31f9f28f7175
henrique007 <dayvsonn.ti@outlook.com>
2022-05-06 22:47:00 -0300

    inicio do projeto mobile

---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+import { styles } from './styles';
+
+export function Widget() {
+  return (
+    <>
+      <TouchableOpacity>
+        
+      </TouchableOpacity>
+    </>
+}
